94cae69c053beb95001de11c226cd7490a48fff0
despoina <depoula@gmail.com>
2022-03-27 19:12:09 +0300

    2

---
+++ Coffee_Machine/oop-coffee-machine-start/main.py
@@ -0,0 +1,26 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+start = True
+menu = Menu()
+
+money_machine.report()
+
+while start:
+    options = menu.get_items()
+    choice = input(f"What you would like? {options}: ")
+    if choice == "off":
+        start = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
